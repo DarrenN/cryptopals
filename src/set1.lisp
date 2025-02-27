eef7f94abb0dd0eaf1ff6e02ea56195f43c70158
DarrenN <info@v25media.com>
2025-02-26 21:13:57 -0500

    Set 1 / Challenge 1

--- src/set1.lisp
+++ src/set1.lisp
@@ -1,8 +1,29 @@
 (defpackage cryptopals/set1
-  (:use :cl))
+  (:use :cl)
+  (:import-from :cl-base64)
+  (:export :hex-string-to-bytes
+           :hex-to-base64))
 
 (in-package :cryptopals/set1)
 
 ;;; Cryptopals Challenge Set 1
 ;;; https://www.cryptopals.com/sets/1
 
+;;; Challenge 1
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun hex-string-to-bytes (hex-string)
+  "Converts a hexadecimal string into a vector of (unsigned-byte 8) values.
+Assumes HEX-STRING contains an even number of hex digits."
+  (let* ((len (length hex-string))
+         (num-bytes (/ len 2))
+         (result (make-array num-bytes :element-type '(unsigned-byte 8))))
+    (loop for i from 0 below num-bytes do
+      (let ((hex-pair (subseq hex-string (* i 2) (+ (* i 2) 2))))
+        (setf (aref result i)
+              (parse-integer hex-pair :radix 16))))
+    result))
+
+(defun hex-to-base64 (hex-string)
+  (base64:usb8-array-to-base64-string
+   (coerce (hex-string-to-bytes hex-string) '(vector (unsigned-byte 8)))))
--- tests/main.lisp
+++ tests/main.lisp
@@ -9,3 +9,15 @@
 (deftest test-target-1
   (testing "should (= 1 1) to be true"
     (ok (= 1 1))))
+
+;; Set 1
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Challenge 1
+
+(deftest day1
+  (testing "hex to base64"
+    (ok (equal
+         (cryptopals/set1:hex-to-base64
+          "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
+         "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"))))

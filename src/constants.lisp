322aa9810f9597d57a6997e094e3cd96ed24f23d
DarrenN <info@v25media.com>
2025-03-30 14:21:15 -0400

    Wraps defconstant with alexandria:define-constant
    To avoid redefition errors
    https://stackoverflow.com/questions/34800988/sbcl-asdfload-system-fails-when-a-string-constant-is-defined

--- src/constants.lisp
+++ src/constants.lisp
@@ -1,13 +1,14 @@
 (defpackage cryptopals/constants
   (:use :cl)
   (:import-from :serapeum #:dict :href)
+  (:import-from :alexandria #:define-constant)
   (:export #:+english-letters+
            #:+english-occurance+
            #:+etaoin-shrdlu+))
 
 (in-package :cryptopals/constants)
 
-(defconstant +english-occurance+
+(define-constant +english-occurance+
   (dict
    "a" 8.2389258     "b" 1.5051398     "c" 2.8065007     "d" 4.2904556
    "e" 12.813865     "f" 2.2476217     "g" 2.0327458     "h" 6.1476691
@@ -15,13 +16,15 @@
    "m" 2.4271893     "n" 6.8084376     "o" 7.5731132     "p" 1.9459884
    "q" 0.0958366     "r" 6.0397268     "s" 6.3827211     "t" 9.1357551
    "u" 2.7822893     "v" 0.9866131     "w" 2.3807842     "x" 0.1513210
-   "y" 1.9913847     "z" 0.0746517))
+   "y" 1.9913847     "z" 0.0746517)
+  :test #'equalp)
 
-(defconstant +english-letters+
+(define-constant +english-letters+
   '("a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s"
-    "t" "u" "v" "x" "y" "z"))
+    "t" "u" "v" "x" "y" "z")
+  :test #'equalp)
 
-(defconstant +etaoin-shrdlu+
+(define-constant +etaoin-shrdlu+
   (dict
    "e" 1
    "t" 1
@@ -35,4 +38,5 @@
    "d" 1
    "l" 1
    "u" 1
-   " " 1))
+   " " 1)
+  :test #'equalp)
